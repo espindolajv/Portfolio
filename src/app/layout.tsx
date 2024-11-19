2f6e6bdac4dd9b7221e28838b80f7f45439babb6
espindolajv <jocanilds@gmail.com>
2024-11-19 12:17:29 -0300

    Theme ajust

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,6 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import "./globals.css";
-import { Schibsted_Grotesk } from 'next/font/google';
 import { ContextProvider } from "@/context/context";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-// ${geistSans.variable} ${geistMono.variable}
-
-const grotesk = Schibsted_Grotesk({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '900']
-})
 
 export const metadata: Metadata = {
   title: "João Victor",
@@ -36,11 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ContextProvider>
-        <body
-          className={`${grotesk.className} subpixel-antialiased overflow-x-hidden`}
-        >
-          {children}
-        </body>
+        {children}
       </ContextProvider>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,29 @@ import { HomeContent } from "./components/HomeContent";
 import { CarouselSection } from "./components/CarouselSection";
 import { TechSection } from "./components/TechSection";
 import { AboutMeSection } from "./components/AboutMeSection";
+import { Schibsted_Grotesk } from 'next/font/google';
 import { useMyContext } from "@/context/context";
+
+const grotesk = Schibsted_Grotesk({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '900']
+})
 
 export default function Home() {
   const { color } = useMyContext()
 
   return (
-    <main className={`${color ? "bg-light-gradient text-zinc-900" : "bg-dark-gradient text-white"} duration-300 bg-color-pattern`}>
-      <HomeContent  />
-      <div className="flex flex-col gap-32">
-        <TechSection />
-        <CarouselSection />
-        <AboutMeSection />
-      </div>
-    </main>
+    <body
+      className={`${grotesk.className} subpixel-antialiased overflow-x-hidden ${color ? 'bg-zinc-100 text-zinc-800' : 'bg-zinc-800 text-white'}`}
+    >
+      <main className={`duration-300`}>
+        <HomeContent />
+        <div className="flex flex-col gap-32">
+          <TechSection />
+          <CarouselSection />
+          <AboutMeSection />
+        </div>
+      </main>
+    </body>
   );
 }
